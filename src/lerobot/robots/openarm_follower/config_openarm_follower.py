079e702890d76a717f744245c9a69b9ac5193cbb
Cota Ops Computer <cota-ops>
2026-06-29 14:21:07 +0200

    altered openarm joint limit config, j2 90 -> 180

--- src/lerobot/robots/openarm_follower/config_openarm_follower.py
+++ src/lerobot/robots/openarm_follower/config_openarm_follower.py
@@ -22,7 +22,7 @@ from ..config import RobotConfig
 
 LEFT_DEFAULT_JOINTS_LIMITS: dict[str, tuple[float, float]] = {
     "joint_1": (-75.0, 75.0),
-    "joint_2": (-90.0, 9.0),
+    "joint_2": (-180.0, 9.0),
     "joint_3": (-85.0, 85.0),
     "joint_4": (0.0, 135.0),
     "joint_5": (-85.0, 85.0),
@@ -33,7 +33,7 @@ LEFT_DEFAULT_JOINTS_LIMITS: dict[str, tuple[float, float]] = {
 
 RIGHT_DEFAULT_JOINTS_LIMITS: dict[str, tuple[float, float]] = {
     "joint_1": (-75.0, 75.0),
-    "joint_2": (-9.0, 90.0),
+    "joint_2": (-9.0, 180.0),
     "joint_3": (-85.0, 85.0),
     "joint_4": (0.0, 135.0),
     "joint_5": (-85.0, 85.0),
